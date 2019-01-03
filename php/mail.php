a137b7cedfc4b025e1cfd4dd4633860a062d9bf5
OzkarVai <oscar.01.02@hotmail.com>
2019-01-02 23:59:58 -0600

    alerta en los mensajes

--- php/mail.php
+++ php/mail.php
@@ -5,18 +5,25 @@ $name = $form[0];
 $email = $form[1];
 $spec = $form[2];
 $phone = $form[3];
-$msg = $form[4];
+if(count($form) > 4){
+    $msg = $form[4];
+}
 /* para que mande el email debe de estar montado en un servido */
 $para = "karen@xtnegocio.com.mx";
 $asunto = "Prueba de SMTP local";
-$mensaje = $name." ".$spec."\r\n".$msg."\r\n".$phone;
+if(count($form) > 4){
+    $mensaje = $name." ".$spec."\r\n".$msg."\r\n".$phone;
+}else{
+    $mensaje = $name." ".$spec."\r\n".$phone;
+}
+
 $cabeceras = "From: ".$email . "\r\n" .
 "X-Mailer: PHP/" . phpversion();
 
 if(mail($para, $asunto, $mensaje, $cabeceras)) {
     echo "Correo enviado correctamente";
 } else {
-    echo "Error al enviar mensaje";
+    echo $mensaje."Error al enviar mensaje";
 }
 
 ?>
